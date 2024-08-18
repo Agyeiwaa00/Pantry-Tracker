e0e71f3fe9683aef9401e4d1f5cedde7375d6d52
Agyeiwaa00 <scoogwinkworth@gmail.com>
2024-08-18 18:22:19 +0000

    update

--- app/firebase.js
+++ app/firebase.js
@@ -9,7 +9,7 @@ import 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCOMex-KOA_r9GcrXXKS33ZOurKCGcQ3oA",
+  apiKey: "",
   authDomain: "eapantryapp.firebaseapp.com",
   projectId: "eapantryapp",
   storageBucket: "eapantryapp.appspot.com",
